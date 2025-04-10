1fdf754355b09ac848b9a08f382864ed3253d832
davideerusso <rd.96.rd@gmail.com>
2025-04-10 17:28:43 +0200

    ticket

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const distanzaImput = document.getElementById("distanza");
 const etaImput = document.getElementById("eta");
 const datiBigliettoForm = document.getElementById("dati-biglietto");
 const alertError = document.getElementById("errore");
+
+const ticket = document.getElementById("ticket");
 const nameCard = document.getElementById("card-name");
 const nameTicket = document.getElementById("card-ticket");
 const cardCarrozza = document.getElementById("card-carrozza");
@@ -16,29 +18,46 @@ datiBigliettoForm.addEventListener("submit", function (event) {
   const distanza = distanzaImput.value;
   const eta = etaImput.value;
 
-  const price = distanza * 0.21 + " €";
+  const priceStandard = distanza * 0.21;
+  const price40 = priceStandard - (distanza * 0.21 * 40) / 100;
 
-  console.log(price);
-
-  const price40 = (distanza * 0.21 * 40) / 100 + " €";
-  const price20 = (distanza * 0.21 * 20) / 100 + " €";
   console.log(price40);
+
+  let typeTicket = "";
+  let price;
 
   let hasError = false;
 
   if (!name) hasError = true;
   if (!distanza || isNaN(distanza)) hasError = true;
   if (!eta) hasError = true;
+  if (!ticket) hasError = true;
 
   if (hasError) {
     alertError.classList.remove("d-none");
+    ticket.classList.add("d-none");
     return;
   }
-
+  ticket.classList.remove("d-none");
   alertError.classList.add("d-none");
 
+  if (eta === "minorenne") {
+    nameTicket.innerHTML = "Ridotto Giovani";
+    price = priceStandard - (distanza * 0.21 * 20) / 100;
+  }
+  if (eta === "maggiorenne") {
+    nameTicket.innerHTML = "Biglietto Standard";
+
+    price = priceStandard;
+  } else if (eta === "over65") {
+    nameTicket.innerHTML = "Ridotto Over";
+    price = priceStandard - (distanza * 0.21 * 40) / 100;
+  }
+  cardPrice.innerText = `${price.toFixed(2) + " €"}`;
   nameCard.innerText = `${name}`;
-  cardPrice.innerText = `${price}`;
+  //   nameTicket.innerText = `${typeTicket}`;
+
+  console.log(price);
 
   console.log("Persona Registrata");
   console.log(" Nome", name);
